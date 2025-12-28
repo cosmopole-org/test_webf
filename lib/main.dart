15c044700ffd39107fbb2f80022485e2812f96a3
Keyhan Mohammadi <theprogrammermachine@gmail.com>
2025-12-28 10:59:45 +0330

    test

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,7 @@
-/*
- * Copyright (C) 2019-2022 The Kraken authors. All rights reserved.
- * Copyright (C) 2022-present The WebF authors. All rights reserved.
- */
 
-// ignore_for_file: avoid_print
 
 import 'package:flutter/material.dart';
-import 'package:webf/rendering.dart';
 import 'package:webf/webf.dart';
-import 'package:webf/devtools.dart';
-import 'package:flutter/cupertino.dart';
 
 final RouteObserver<ModalRoute<void>> routeObserver =
     RouteObserver<ModalRoute<void>>();
@@ -18,11 +10,10 @@ final GlobalKey<NavigatorState> navigatorKey = GlobalKey<NavigatorState>();
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
-  // Initialize the controller manager
   WebFControllerManager.instance.initialize(
     WebFControllerManagerConfig(
-      maxAliveInstances: 4,
-      maxAttachedInstances: 1,
+      maxAliveInstances: 5,
+      maxAttachedInstances: 3,
       onControllerDisposed: (String name, WebFController controller) {
         print('controller disposed: $name $controller');
       },
@@ -34,11 +25,9 @@ void main() async {
 
   WebFControllerManager.instance.addWithPreload(
     name: 'home',
-    createController: () => WebFController(
-      bundle: WebFBundle.fromUrl('https://miracleplus.openwebf.com/'),
-    ),
+    createController: () => WebFController(),
     bundle: WebFBundle.fromContent(
-      "<html><head></head><body><div>test</div></body></html>",
+      "<html><head></head><body><h1>test</h1></body></html>",
     ),
   );
 
@@ -51,35 +40,14 @@ class MyApp extends StatefulWidget {
 }
 
 class MyAppState extends State<MyApp> {
-  WebFController? controller;
-
-  @override
-  void initState() {
-    super.initState();
-    WebFControllerManager.instance.getController("home").then((cont) {
-      setState(() {
-        controller = cont;
-      });
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
-    if (controller != null) {
-      return Scaffold(
-        appBar: AppBar(title: Text("Webf Test")),
-        body: WebFHTMLElement(
-          tagName: "h1",
-          controller: controller!,
-          parentElement: null,
-          children: [Text("hello world !")],
-        ),
-      );
-    } else {
-      return Container(
-        padding: EdgeInsets.all(16),
-        child: Text("controller not found"),
-      );
-    }
+    return Scaffold(
+      appBar: AppBar(title: Text("Webf Test")),
+      body: WebF.fromControllerName(
+        controllerName: 'home',
+        loadingWidget: CircularProgressIndicator(),
+      ),
+    );
   }
 }
